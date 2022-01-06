416f4bca6f11b6d79d7796637db3ef386a825fa5
DeusFerrariis <cleghornw@protonmail.com>
2022-01-05 18:53:32 -0700

    removed code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,4 @@ use warp::Filter;
 
 #[tokio::main]
  async fn main() {
-                //GET Endpoints
-     let list_peers =  warp::path("l_peers")
-         .and(warp::get())
-         //.and(do_auth())
-         //security check filter
-         .map(|| "list of peers - path success")
-         .and_then(get_peer_list);
-        }
-
-
-     //let peer_listing = warp::path("p_list")
-     //.and(warp::get())
-     //.map(|| "each peer listing - path success");
-
-     let get_EP = list_peers;
-     //.or(peer_listing);
-
-
-    async fn get_peer_list(_user_ctx: UserCtx, id: i64)
-    -> Result<Json, warp::Rejection> {
-        //TODO: Get and return peer list
-        //There should be a handler function w a
-        //hard coded list of peers from peer.rs
-
-
-    }
-
-     let routes = get_ep; //or(post_endpoints);
-     println!("API Running");
-     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
-     //running local host till further notice
  }
