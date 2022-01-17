814afd712ad646beee97f92d75ec536e7051d6c2
DeusFerrariis <cleghornw@protonmail.com>
2022-01-17 15:36:35 -0700

    added ws_rpc endpoint with support for inserting an rpc dynamically

--- src/serve/filters/mod.rs
+++ src/serve/filters/mod.rs
@@ -1,37 +1,59 @@
 use super::super::peer::*;
-use futures_util::{FutureExt, StreamExt};
-use warp::filters::BoxedFilter;
-use warp::reply::Json;
-use warp::Filter;
+use futures_util::{TryFutureExt, SinkExt, StreamExt};
+use jsonrpc_core::IoHandler;
+use tokio::sync::mpsc;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use warp::{
+    ws::{Message, WebSocket},
+    Filter,
+};
 
-pub fn ws_rpc() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Copy {
+pub fn rpc(
+    rpc_handler: IoHandler,
+) -> impl Filter<Extract = (IoHandler,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || rpc_handler.clone())
+}
+
+pub fn ws_rpc(
+    rpc_handler: IoHandler,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("ws_rpc")
         .and(warp::ws())
+        .and(rpc(rpc_handler))
         .and_then(ws_rpc_handler)
 }
 
 pub async fn ws_rpc_handler(
     ws: warp::ws::Ws,
-) -> Result<impl warp::Reply, std::convert::Infallible> {
-    Ok(ws.on_upgrade(move |socket| {
-        // TODO:
-        // - add clients as peers
-        // - when data is recieved pass it to rpc handler
+    handler: IoHandler,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(ws.on_upgrade(move |socket| async move {
+        let rpc_handler = handler.clone();
 
-        // tx -> send stream, rx -> recieve stream
-        let (tx, rx) = socket.split();
+        let (mut client_ws_sender, mut client_ws_reciever) = socket.split();
 
-        // forwards data sent back to sender
-        rx.forward(tx).map(|res| {
-            if let Err(e) = res {
-                println!("websocket error: {}", e);
+        while let Some(res) = client_ws_reciever.next().await {
+            match res {
+                Ok(message) => {
+                    let rpc_response = rpc_handler.handle_request(message.to_str().unwrap()).await.unwrap_or(String::from("Couldn't process rpc request"));
+
+                    client_ws_sender.send(Message::text(rpc_response)).unwrap_or_else(|e| {
+                        println!("Error sending to client: {}", e);
+                    }).await;
+                },
+                Err(e) => {
+                    let error_message = Message::text(format!("{:?}", e));
+                    client_ws_sender.send(error_message).unwrap_or_else(|e| {
+                        println!("Error sending to client: {}", e);
+                    }).await;
+                }
             }
-        })
+        }
     }))
 }
 
-// pub fn handle_peer_ws(ws: war) {//for peers that are connected}
-pub fn list_peers() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Copy {
+pub fn list_peers() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("l_peers")
         .and(warp::get())
         .and_then(get_peer_list)
@@ -52,8 +74,14 @@ pub async fn get_peer_list() -> Result<impl warp::Reply, std::convert::Infallibl
 
 #[cfg(test)]
 mod test {
-    use super::super::super::peer::*;
-    use super::{list_peers, ws_rpc};
+    use super::{
+        super::super::peer::*,
+        list_peers,
+        ws_rpc,
+    };
+    use jsonrpc_core::{IoHandler, Result};
+    use jsonrpc_derive::rpc;
+    use tokio::sync::Mutex;
 
     #[tokio::test]
     async fn test_get_peer_list() {
@@ -84,22 +112,37 @@ mod test {
         }
     }
 
+    #[rpc]
+    pub trait SampleRpc {
+        #[rpc(name = "hello_world")]
+        fn hello_world(&self) -> Result<String>;
+    }
+
+    pub struct SampleProtocol;
+
+    impl SampleRpc for SampleProtocol {
+        fn hello_world(&self) -> Result<String> {
+            Ok("Hello World!".to_string())
+        }
+    }
+
     #[tokio::test]
-    async fn test_ws_rpc() {
+    async fn test_rpc_ws() {
+        let mut rpc_handler = IoHandler::new();
+        rpc_handler.extend_with(SampleProtocol.to_delegate());
+
         let mut res = warp::test::ws()
             .path("/ws_rpc")
-            .handshake(ws_rpc())
+            .handshake(ws_rpc(rpc_handler))
             .await
             .expect("handshake");
 
         // TODO: test that rpc endpoints are accessible
 
-        let _send = &res.send_text("bing_bong").await;
-
+        let _send = &res.send_text(r#"{"jsonrpc": "2.0", "method": "hello_world", "id": 1}"#).await;
         let rec = res.recv().await.unwrap();
-
         let rec_string = rec.to_str().unwrap();
 
-        assert_eq!("bing_bong", rec_string);
+        assert!(rec_string.contains("Hello World!"));
     }
 }
--- src/serve/handlers/mod.rs
+++ src/serve/handlers/mod.rs
@@ -1,5 +1,34 @@
-// use warp::{ ws::WebSocket, Reply };
-//
-// pub async fn handle_peer_ws(ws: WebSocket) -> warp::ws::Result<impl Reply> {
-//     // TODO:
-// }
+use jsonrpc_core::IoHandler;
+use futures_util::{FutureExt, SinkExt, StreamExt};
+use warp::ws::{Message, WebSocket};
+
+
+/*
+pub async fn ws_rpc_handler(
+    ws: warp::ws::Ws,
+    handler: IoHandler,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(ws.on_upgrade(move |socket| async {
+        let (mut client_ws_sender, mut client_ws_reciever) = socket.split();
+
+        while let Some(res) = client_ws_reciever.next().await {
+            match res {
+                Ok(message) => {
+                    let rpc_response = &handler.handle_request(message.to_str().unwrap());
+                    match client_ws_sender.send(Message::text("Ree")).await {
+                        Err(e) => println!("Error sending to client: {}", e),
+                        _ => {}
+                    }
+                },
+                Err(e) => {
+                    let error_message = Message::text(format!("{:?}", e));
+                    match client_ws_sender.send(error_message).await {
+                        Err(e) => println!("Error sending to client: {}", e),
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }))
+}
+*/
