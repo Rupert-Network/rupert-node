b55a49021c65e5230523c93b497da502255e14ff
DeusFerrariis <cleghornw@protonmail.com>
2022-02-08 23:06:06 -0700

    added comments to rpc_test

--- rpc_test.go
+++ rpc_test.go
@@ -6,15 +6,21 @@ import (
 )
 
 func TestVerify(t *testing.T) {
+	// Create new Verify
 	v := new(Verify)
+	// Empty account until read from chain implemented
 	args := struct{ account []byte }{[]byte(":)")}
+	// Byte array to store response
 	var reply []byte
 
+	// Call
 	v.GetEncryptedMessage(
 		&args,
 		&reply,
 	)
 
+	// As GetEncryptedMessage is incomplete this is just to demonstrate the
+	// passing of the message back
 	if string(reply) != ":(" {
 		t.Fatalf("Crazy it was actually %s",
 			base64.StdEncoding.EncodeToString(reply),
